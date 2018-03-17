c63e36f4f7b9d7c0029ee4a742830d7662e6b062
Kay Mok <kaymok3@gmail.com>
2018-03-17 13:16:24 -0400

    Initial commit

---
+++ scripts/fellowship.js
@@ -0,0 +1,21 @@
+console.log("LINKED");
+
+// Dramatis Personae
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
+
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+// Our Setting
+var lands = ['The Shire', 'Rivendell', 'Mordor'];
+// Starter code for the body
+var body = document.querySelector('body');
